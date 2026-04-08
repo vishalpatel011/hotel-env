d12bddd0330088311c948642b00adcb937b6d6c2
Vishal Patel <patelv254@gmail.com>
2026-04-09 03:24:01 +0530

    fix: add tasks module for validator

---
+++ env/tasks.py
@@ -0,0 +1,16 @@
+from env.grader import grade_easy, grade_medium, grade_hard
+
+tasks = [
+    {
+        "name": "easy",
+        "grader": grade_easy,
+    },
+    {
+        "name": "medium",
+        "grader": grade_medium,
+    },
+    {
+        "name": "hard",
+        "grader": grade_hard,
+    }
+]

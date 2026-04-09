af8326e8b9793b5804992668e29b6cc77f613e15
Vishal Patel <patelv254@gmail.com>
2026-04-09 19:54:06 +0530

    fix: add grader endpoints

---
+++ grader.py
@@ -0,0 +1,3 @@
+from env.grader import grade_easy, grade_medium, grade_hard
+
+__all__ = ["grade_easy", "grade_medium", "grade_hard"]
---
+++ tasks.py
@@ -0,0 +1,3 @@
+from env.tasks import TASKS, TASK_GRADERS, tasks
+
+__all__ = ["tasks", "TASKS", "TASK_GRADERS"]
